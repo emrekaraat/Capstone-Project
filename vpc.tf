065f93235d8c81c69802a229257c25aebda03eff
emrekaraat <emrekaraat@gmail.com>
2025-04-20 20:47:44 +0200

    Set AWS provider region to us-west-2

---
+++ vpc.tf
@@ -0,0 +1,11 @@
+# Creating VPC
+
+resource "aws_vpc" "capstone" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "capstone-vpc"
+  }
+}
