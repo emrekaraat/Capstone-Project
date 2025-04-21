29a05eca0b2df91388e0db3c425b24debb56a393
emrekaraat <emrekaraat@gmail.com>
2025-04-21 09:28:46 +0200

    Defining input variables: AMI ID, instance type, key pair and WordPress DB & MariaDB Passwords

--- variables.tf
+++ variables.tf
@@ -1,3 +1,4 @@
+# EC2 Configuration Variables
 variable "ami_id" {
   description = "AMI to use for EC2 instance"
   default     = "ami-087f352c165340ea1"
@@ -11,4 +12,22 @@ variable "instance_type" {
 variable "key_name" {
   description = "Key pair name for EC2"
   default     = "vockey"
+}
+
+# WordPress DB & MariaDB Passwords
+variable "db_root_password" {
+  description = "MariaDB root password"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_user" {
+  description = "WordPress DB username"
+  type        = string
+}
+
+variable "db_user_password" {
+  description = "Password for the WordPress database user"
+  type        = string
+  sensitive   = true
 }
