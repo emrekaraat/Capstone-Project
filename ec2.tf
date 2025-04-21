b4bb15f8605f50a3f9988f84880bd2a3602d1130
emrekaraat <emrekaraat@gmail.com>
2025-04-21 09:27:38 +0200

    Launching EC2 instance with user data script

--- ec2.tf
+++ ec2.tf
@@ -6,7 +6,11 @@ resource "aws_instance" "capstone" {
   vpc_security_group_ids      = [aws_security_group.capstone_sg.id]
   associate_public_ip_address = true
 
-  user_data = file("user_data.sh")
+  user_data = templatefile("${path.module}/user_data.sh", {
+    db_root_password   = var.db_root_password,
+    db_user            = var.db_user,
+    db_user_password   = var.db_user_password
+  })
 
   tags = {
     Name = "capstone-ec2"
