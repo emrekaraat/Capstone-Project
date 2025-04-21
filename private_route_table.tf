e3490e231ec1ad77c3982a6a562e6338ec36f8ef
emrekaraat <emrekaraat@gmail.com>
2025-04-21 14:19:28 +0200

    Update private route table to use NAT Gateway for internet access

--- private_route_table.tf
+++ private_route_table.tf
@@ -1,3 +1,5 @@
+#creating private_route_table
+
 resource "aws_route_table" "private_route_table" {
   vpc_id = aws_vpc.capstone.id
 
@@ -15,3 +17,4 @@ resource "aws_route_table_association" "private_route_table_assoc" {
   subnet_id      = aws_subnet.capstone_private.id
   route_table_id = aws_route_table.private_route_table.id
 }
+
