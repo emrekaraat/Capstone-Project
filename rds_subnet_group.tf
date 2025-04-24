f2a75cfd21d982c89a339ce81b25f9477e536dbf
emrekaraat <emrekaraat@gmail.com>
2025-04-24 14:28:14 +0200

    Update RDS subnet group to include both private subnets

--- rds_subnet_group.tf
+++ rds_subnet_group.tf
@@ -1,6 +1,9 @@
 resource "aws_db_subnet_group" "capstone_subnet_group" {
   name       = "capstone-subnet-group"
-  subnet_ids = [aws_subnet.capstone_private.id]
+  subnet_ids = [
+    aws_subnet.capstone_private_1.id,
+    aws_subnet.capstone_private_2.id
+  ]
 
   tags = {
     Name = "capstone-rds-subnet-group"
