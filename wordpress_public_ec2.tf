2b4849e0c28af1f4007eb72f0e172cb0a199872a
emrekaraat <emrekaraat@gmail.com>
2025-04-24 13:55:07 +0200

    Enable RDS endpoint usage in WordPress EC2 instance

--- wordpress_public_ec2.tf
+++ wordpress_public_ec2.tf
@@ -9,7 +9,9 @@ resource "aws_instance" "capstone" {
   user_data = templatefile("${path.module}/user_data.sh.tpl", {
     db_root_password = var.db_root_password,
     db_user          = var.db_user,
-    db_user_password = var.db_user_password
+    db_user_password = var.db_user_password,
+    db_name          = var.db_name,
+    db_host          = aws_db_instance.capstone_rds.endpoint
   })
 
   tags = {
