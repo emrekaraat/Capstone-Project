32e1285826d9bbbc635db22035220f89e11933c6
emrekaraat <emrekaraat@gmail.com>
2025-04-24 15:23:12 +0200

    Re-enable EC2 and WordPress output variables

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
-# output "ec2_public_ip" {
-#   description = "The public IP of the EC2 instance"
-#   value       = aws_instance.capstone.public_ip
-# }
+output "ec2_public_ip" {
+  description = "The public IP of the EC2 instance"
+  value       = aws_instance.capstone.public_ip
+}
 
-# output "wordpress_setup_url" {
-#   description = "URL for WordPress setup"
-#   value       = "http://${aws_instance.capstone.public_ip}/"
-# }
+output "wordpress_setup_url" {
+  description = "URL for WordPress setup"
+  value       = "http://${aws_instance.capstone.public_ip}/"
+}
 
-# output "php_info_url" {
-#   description = "URL to check PHP info page"
-#   value       = "http://${aws_instance.capstone.public_ip}/info.php"
-# }
+output "php_info_url" {
+  description = "URL to check PHP info page"
+  value       = "http://${aws_instance.capstone.public_ip}/info.php"
+}
