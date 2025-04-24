cb4692267f696faf4872e9daa9e0468d5ff56de8
emrekaraat <emrekaraat@gmail.com>
2025-04-24 13:42:58 +0200

    Add RDS endpoint output for WordPress DB connection

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,8 @@ output "php_info_url" {
   description = "URL to check PHP info page"
   value       = "http://${aws_instance.capstone.public_ip}/info.php"
 }
+
+output "rds_endpoint" {
+  description = "The RDS endpoint to be used as DB_HOST in WordPress"
+  value       = aws_db_instance.capstone_rds.endpoint
+}
