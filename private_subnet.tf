fa0c16a79138bd515a933c140218a729fb65fa27
emrekaraat <emrekaraat@gmail.com>
2025-04-24 14:28:04 +0200

    Add second private subnet in us-west-2b for RDS AZ coverage

--- private_subnet.tf
+++ private_subnet.tf
@@ -1,10 +1,21 @@
-resource "aws_subnet" "capstone_private" {
+resource "aws_subnet" "capstone_private_1" {
   vpc_id                  = aws_vpc.capstone.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "us-west-2a"
   map_public_ip_on_launch = false
 
   tags = {
-    Name = "capstone-private-subnet"
+    Name = "capstone-private-subnet-1"
+  }
+}
+
+resource "aws_subnet" "capstone_private_2" {
+  vpc_id                  = aws_vpc.capstone.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "us-west-2b"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "capstone-private-subnet-2"
   }
 }
