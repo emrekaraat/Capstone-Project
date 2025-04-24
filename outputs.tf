f9508de1c37348652bd634ffb227d02c0e57931b
emrekaraat <emrekaraat@gmail.com>
2025-04-24 22:30:59 +0200

    Add detailed outputs for Bastion IP, RDS endpoint, and local/bastion WordPress URLs

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,41 @@
-output "ec2_public_ip" {
-  description = "The public IP of the EC2 instance"
-  value       = aws_instance.capstone.public_ip
+# Bastion Host Public IP
+output "bastion_public_ip" {
+  description = "Public IP of the Bastion Host"
+  value       = aws_instance.bastion_host.public_ip
 }
 
-output "wordpress_setup_url" {
-  description = "URL for WordPress setup"
-  value       = "http://${aws_instance.capstone.public_ip}/"
+# WordPress EC2 Private IP
+output "wordpress_private_ip" {
+  description = "Private IP of the WordPress EC2 instance"
+  value       = aws_instance.wordpress.private_ip
 }
 
-output "php_info_url" {
-  description = "URL to check PHP info page"
-  value       = "http://${aws_instance.capstone.public_ip}/info.php"
+# RDS Endpoint
+output "rds_endpoint" {
+  description = "Endpoint of the RDS instance"
+  value       = aws_db_instance.capstone_rds.endpoint
+}
+
+# phpinfo URL - via SSH port forwarding
+output "php_info_url_localhost" {
+  description = "phpinfo page via localhost:8080 through SSH tunnel"
+  value       = "http://localhost:8080/info.php"
+}
+
+# phpinfo URL - via Bastion Public IP
+output "php_info_url_bastion" {
+  description = "phpinfo page via Bastion public IP"
+  value       = "http://${aws_instance.bastion_host.public_ip}/info.php"
+}
+
+# WordPress Setup URL - via localhost (tunnel)
+output "wordpress_setup_url_localhost" {
+  description = "WordPress setup page via SSH tunnel"
+  value       = "http://localhost:8080"
+}
+
+# WordPress Setup URL - via Bastion Public IP
+output "wordpress_setup_url_bastion" {
+  description = "WordPress setup page via Bastion public IP"
+  value       = "http://${aws_instance.bastion_host.public_ip}"
 }
