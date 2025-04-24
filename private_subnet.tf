925dd433cb2b6fb01cd95bdecc8a2deee1d9c563
emrekaraat <emrekaraat@gmail.com>
2025-04-24 14:39:19 +0200

    Fix: Update CIDR blocks for private subnets to avoid conflict

--- private_subnet.tf
+++ private_subnet.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "capstone_private_1" {
   vpc_id                  = aws_vpc.capstone.id
-  cidr_block              = "10.0.2.0/24"
+  cidr_block              = "10.0.10.0/24"  # Updated
   availability_zone       = "us-west-2a"
   map_public_ip_on_launch = false
 
@@ -11,7 +11,7 @@ resource "aws_subnet" "capstone_private_1" {
 
 resource "aws_subnet" "capstone_private_2" {
   vpc_id                  = aws_vpc.capstone.id
-  cidr_block              = "10.0.3.0/24"
+  cidr_block              = "10.0.11.0/24"  # Updated
   availability_zone       = "us-west-2b"
   map_public_ip_on_launch = false
 
