c953f9ce35b467739e1a1640b4de1eae0009ef5b
emrekaraat <emrekaraat@gmail.com>
2025-04-24 14:52:36 +0200

    Temporarily disable EC2 and WordPress output variables to avoid 'Known after apply' issues

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,14 @@
-output "ec2_public_ip" {
-  description = "The public IP of the EC2 instance"
-  value       = aws_instance.capstone.public_ip
-}
+# output "ec2_public_ip" {
+#   description = "The public IP of the EC2 instance"
+#   value       = aws_instance.capstone.public_ip
+# }
 
-output "wordpress_setup_url" {
-  description = "URL for WordPress setup"
-  value       = "http://${aws_instance.capstone.public_ip}/"
-}
+# output "wordpress_setup_url" {
+#   description = "URL for WordPress setup"
+#   value       = "http://${aws_instance.capstone.public_ip}/"
+# }
 
-output "php_info_url" {
-  description = "URL to check PHP info page"
-  value       = "http://${aws_instance.capstone.public_ip}/info.php"
-}
-
-output "rds_endpoint" {
-  description = "The RDS endpoint to be used as DB_HOST in WordPress"
-  value       = aws_db_instance.capstone_rds.endpoint
-}
+# output "php_info_url" {
+#   description = "URL to check PHP info page"
+#   value       = "http://${aws_instance.capstone.public_ip}/info.php"
+# }
