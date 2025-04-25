4b24825cecb625b5d9be84ef816cdb05094e4670
emrekaraat <emrekaraat@gmail.com>
2025-04-25 16:13:54 +0200

    Retry: Clean start after S3 bucket deletion for backend setup

--- s3_backend_setup.tf
+++ s3_backend_setup.tf
@@ -1,7 +1,6 @@
 # S3 Bucket for Terraform state
 resource "aws_s3_bucket" "terraform_state" {
-  bucket              = "capstone-terraform-emre-3501"
-  object_lock_enabled = false  # Object lock'u devre dışı bırakıyoruz
+  bucket = "capstone-terraform-emre-3501"
 
   tags = {
     Name        = "Capstone Terraform State"
@@ -38,21 +37,4 @@ resource "aws_s3_bucket_public_access_block" "terraform_state_block" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
-}
-
-# DynamoDB Table for state locking
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "capstone-terraform-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = {
-    Name        = "Capstone Terraform Locks"
-    Environment = "Dev"
-  }
 }
