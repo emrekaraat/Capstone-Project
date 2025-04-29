8d870dffaf6c288d4977811257a163fa0340fb81
emrekaraat <emrekaraat@gmail.com>
2025-04-29 16:20:40 +0200

    Add Auto Scaling scale-out and and connect to alarm

--- auto_scaling.tf
+++ auto_scaling.tf
@@ -43,3 +43,21 @@ resource "aws_autoscaling_group" "capstone_asg" {
     propagate_at_launch = true
   }
 }
+
+# Scale Out Policy (EC2 sayısını artırır)
+resource "aws_autoscaling_policy" "scale_out_policy" {
+  name                   = "capstone-scale-out"
+  scaling_adjustment     = 1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = aws_autoscaling_group.capstone_asg.name
+}
+
+# Scale In Policy (EC2 sayısını azaltır)
+resource "aws_autoscaling_policy" "scale_in_policy" {
+  name                   = "capstone-scale-in"
+  scaling_adjustment     = -1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = aws_autoscaling_group.capstone_asg.name
+}
--- cloudwatch_alarm.tf
+++ cloudwatch_alarm.tf
@@ -4,11 +4,16 @@ resource "aws_cloudwatch_metric_alarm" "cpu_high_alarm" {
   evaluation_periods  = 2
   metric_name         = "CPUUtilization"
   namespace           = "AWS/EC2"
-  period              = 60
+  period              = 30
   statistic           = "Average"
   threshold           = 70
   alarm_description   = "This metric monitors if CPU usage exceeds 70%."
-  alarm_actions       = [aws_sns_topic.capstone_alarm_topic.arn] # Trigger email notification via SNS
+  
+  alarm_actions = [
+    aws_autoscaling_policy.scale_out_policy.arn,
+    aws_sns_topic.capstone_alarm_topic.arn
+  ]
+
   dimensions = {
     InstanceId = aws_instance.capstone.id
   }
