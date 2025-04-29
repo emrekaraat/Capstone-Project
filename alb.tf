1dba6efba1b27d91875d8708b1b1db9e6f7b81c7
emrekaraat <emrekaraat@gmail.com>
2025-04-29 14:54:22 +0200

    Add ALB with Target Group and Auto Scaling Group

---
+++ alb.tf
@@ -0,0 +1,40 @@
+# Create ALB
+resource "aws_lb" "capstone_alb" {
+  name               = "capstone-alb"
+  load_balancer_type = "application"
+  subnets            = [aws_subnet.capstone_public.id]
+  security_groups    = [aws_security_group.capstone_sg.id]
+
+  tags = {
+    Name = "capstone-alb"
+  }
+}
+
+# Create Target Group
+resource "aws_lb_target_group" "capstone_target_group" {
+  name     = "capstone-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.capstone.id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
+}
+
+# Create Listener
+resource "aws_lb_listener" "capstone_listener" {
+  load_balancer_arn = aws_lb.capstone_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.capstone_target_group.arn
+  }
+}
---
+++ auto_scaling.tf
@@ -0,0 +1,45 @@
+# Launch Template for Auto Scaling
+resource "aws_launch_template" "capstone_launch_template" {
+  name_prefix   = "capstone-launch-template-"
+  image_id      = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  network_interfaces {
+    associate_public_ip_address = false
+    security_groups             = [aws_security_group.capstone_sg.id]
+    subnet_id                   = aws_subnet.capstone_private_1.id
+  }
+
+  user_data = base64encode(templatefile("${path.module}/user_data.sh.tpl", {
+    db_root_password = var.db_root_password,
+    db_user          = var.db_user,
+    db_user_password = var.db_user_password,
+    db_name          = var.db_name,
+    db_host          = aws_db_instance.capstone_rds.endpoint
+  }))
+}
+
+# Auto Scaling Group
+resource "aws_autoscaling_group" "capstone_asg" {
+  name                      = "capstone-asg"
+  max_size                  = 3
+  min_size                  = 1
+  desired_capacity          = 1
+  vpc_zone_identifier       = [aws_subnet.capstone_private_1.id, aws_subnet.capstone_private_2.id]
+  health_check_type         = "ELB"
+  health_check_grace_period = 300
+
+  launch_template {
+    id      = aws_launch_template.capstone_launch_template.id
+    version = "$Latest"
+  }
+
+  target_group_arns = [aws_lb_target_group.capstone_target_group.arn]
+
+  tag {
+    key                 = "Name"
+    value               = "capstone-auto-instance"
+    propagate_at_launch = true
+  }
+}
