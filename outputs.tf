c1255c02c75275e8d3c720c398c63b6e2c9e52db
emrekaraat <emrekaraat@gmail.com>
2025-05-08 14:55:35 +0200

    Clean up: Remove static EC2 IP output for Auto Scaling compatibility

--- outputs.tf
+++ outputs.tf
@@ -4,12 +4,6 @@ output "bastion_public_ip" {
   value       = aws_instance.bastion.public_ip
 }
 
-# WordPress EC2 Private IP
-output "wordpress_private_ip" {
-  description = "Private IP of the WordPress EC2"
-  value       = aws_instance.capstone.private_ip
-}
-
 # RDS Database Endpoint
 output "rds_endpoint" {
   description = "RDS Database Endpoint"
@@ -21,3 +15,11 @@ output "alb_dns" {
   description = "DNS name of the Application Load Balancer"
   value       = aws_lb.capstone_alb.dns_name
 }
+
+# Note: Auto Scaling instances do not have a stable private IP.
+# If you're still using aws_instance.capstone (i.e., static EC2), uncomment the following:
+
+# output "wordpress_private_ip" {
+#   description = "Private IP of the WordPress EC2"
+#   value       = aws_instance.capstone.private_ip
+# }
