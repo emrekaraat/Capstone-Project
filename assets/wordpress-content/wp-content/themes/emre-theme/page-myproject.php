009879a3f40f4abc2c51da0c148455658c6393f3
emrekaraat <emrekaraat@gmail.com>
2025-05-09 15:35:32 +0200

    Update: clean page and center layout

--- assets/wordpress-content/wp-content/themes/emre-theme/page-myproject.php
+++ assets/wordpress-content/wp-content/themes/emre-theme/page-myproject.php
@@ -7,15 +7,6 @@ get_header(); ?>
 <main id="main" class="site-main">
     <section>
         <?php the_content(); ?>
-
-        <h2>Galatasaray Logo</h2>
-        <img src="<?php echo get_template_directory_uri(); ?>/themes/twentytwentyfive/images/gs-logo.jpg" alt="GS Logo" width="300">
-
-        <h2>UEFA Cup Victory</h2>
-        <img src="<?php echo get_template_directory_uri(); ?>/themes/twentytwentyfive/images/gs-uefa-cup.jpg" alt="UEFA Cup" width="300">
-
-        <h2>Me at the Stadium</h2>
-        <img src="<?php echo get_template_directory_uri(); ?>/themes/twentytwentyfive/images/gs-emre-1.jpg" alt="Emre at Stadium" width="300">
     </section>
 </main>
 
